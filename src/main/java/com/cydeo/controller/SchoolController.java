a65a6647e27ca2de24abd93806b1ba3e7f493021
Erkan <ErkanOzulu@users.noreply.github.com>
2024-01-07 21:09:15 +0300

    Parents endpoint is created

--- src/main/java/com/cydeo/controller/SchoolController.java
+++ src/main/java/com/cydeo/controller/SchoolController.java
@@ -1,10 +1,11 @@
 package com.cydeo.controller;
 
-import com.cydeo.dto.StudentDTO;
-import com.cydeo.dto.StudentWrapper;
+import com.cydeo.dto.ResponseWrapper;
 import com.cydeo.dto.TeacherDTO;
+import com.cydeo.service.ParentService;
 import com.cydeo.service.StudentService;
 import com.cydeo.service.TeacherService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,10 +17,12 @@ public class SchoolController {
 
     private final TeacherService teacherService;
     private final StudentService studentService;
+    private final ParentService parentService;
 
-    public SchoolController(TeacherService teacherService, StudentService studentService) {
+    public SchoolController(TeacherService teacherService, StudentService studentService, ParentService parentService) {
         this.teacherService = teacherService;
         this.studentService = studentService;
+        this.parentService = parentService;
     }
 
     @GetMapping("/teachers")
@@ -36,9 +39,27 @@ public class SchoolController {
      */
 
     @GetMapping("students")
-    public ResponseEntity<StudentWrapper> allStudents(){
+    public ResponseEntity<ResponseWrapper> readAllStudents(){
 
       return   ResponseEntity
-              .ok(new StudentWrapper("students are successfully retrieved.",studentService.findAll()));
+              .ok(new ResponseWrapper("students are successfully retrieved.",studentService.findAll()));
+    }
+
+     /*
+           create a parents endpoint where status code is 202
+           additional header has "Parent" , "Returned"
+           and following body structure
+           "parents are successfully retrieved." message
+           code:202
+           success: true
+           and parent data.
+     */
+
+    @GetMapping("/parents")
+    public ResponseEntity<ResponseWrapper> readAllParents(){
+        return ResponseEntity
+                .status(HttpStatus.ACCEPTED)
+                .header("parent","Returned")
+                .body(new ResponseWrapper(true,"parents are successfully retrieved.", HttpStatus.ACCEPTED.value(), parentService.findAll()));
     }
 }
--- src/main/java/com/cydeo/dto/ResponseWrapper.java
+++ src/main/java/com/cydeo/dto/ResponseWrapper.java
@@ -4,29 +4,27 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Component;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-public class StudentWrapper {
+public class ResponseWrapper {
 
     private boolean success;
     private String message;
     private Integer code;
     private Object data;
 
-    public StudentWrapper(String message, Object data) {
+    public ResponseWrapper(String message, Object data) {
         this.message = message;
         this.data = data;
         this.success=true;
         this.code= HttpStatus.OK.value();
     }
 
-    public StudentWrapper(String message) {
+    public ResponseWrapper(String message) {
         this.message = message;
         this.success=true;
         this.code=HttpStatus.OK.value();
